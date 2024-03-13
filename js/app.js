25f85a2c18c6acf71809f82b72f7e10828d85e69
alexutm123 <pilich.sasha@gmail.com>
2024-03-13 11:27:23 +0300

    Optimiz

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-	var lazyImages = document.querySelectorAll("div.slider__layer[data-src]");
-	var observer = new IntersectionObserver(function(entries, observer) {
-		entries.forEach(function(entry) {
-			if (entry.isIntersecting) {
-				var image = entry.target;
-				image.style.backgroundImage = 'url(' + image.getAttribute('data-src') + ')';
-				image.classList.add('visible');
-				observer.unobserve(image);
-			}
-		});
-	}, { rootMargin: "0px 0px 50px 0px" });
-
-	lazyImages.forEach(function(image) {
-		observer.observe(image);
-	});
-});
 
 
 new Swiper('.slider', {
